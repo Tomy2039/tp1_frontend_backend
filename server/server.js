2d7153e8db7deac653bcbc61a6e644a7f1623dcc
Tomy2039 <tramosyahari@gmail.com>
2025-04-07 11:15:59 -0300

    esta hecho el servidor

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.get('/saludo/:nombre', (req, res) => {
+    const nombre = req.params.nombre;
+    res.send(`Hola, ${nombre}!`);
+});
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en el puerto ${port}`)
+})
